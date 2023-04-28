1f34cf24613d0a79da243a456219f56214b7db5c
Brian Quilinan <81407102+Brian150798@users.noreply.github.com>
2023-04-28 12:54:38 -0400

    integracion api

--- core/views.py
+++ core/views.py
@@ -1,6 +1,16 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 
 def indexPage(request):
-    return render(request,'core/index.html')
+
+    url = "https://638cc70fd2fc4a058a5fbbdc.mockapi.io/s"
+
+    servicios = requests.request("GET", url)
+    servicios = servicios.json()
+
+    dato = {
+        'servicios': servicios
+    }
+    return render(request,'core/index.html',dato)
